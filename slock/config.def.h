8be3a78ca5376d4a7eefb6e78637c744a884dd26
GG <weebcyberpunk@gmail.com>
2022-06-01 18:53:11 -0300

    Changed slock to use blur instead of pixelating

--- slock/config.def.h
+++ slock/config.def.h
@@ -30,10 +30,10 @@ static XRectangle rectangles[9] = {
 };
 
 /*Enable blur*/
-/* #define BLUR */
+#define BLUR
 /*Set blur radius*/
-static const int blurRadius=5;
+static const int blurRadius=20;
 /*Enable Pixelation*/
-#define PIXELATION
+/* #define PIXELATION */
 /*Set pixelation radius*/
 static const int pixelSize=27;
