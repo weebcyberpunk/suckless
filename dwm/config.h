8a788e30abbda22c8656735ca33b36f1b85fc342
GG <weebcyberpunk@gmail.com>
2022-06-30 23:33:12 -0300

    Bottom bar because it looks like I have more screen real estate

--- dwm/config.h
+++ dwm/config.h
@@ -6,7 +6,7 @@
 static const unsigned int borderpx  = 4;        /* border pixel of windows */
 static const unsigned int snap      = 12;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const Bool viewontag         = True;     /* Switch view on tag switch */
 static const char *fonts[]          = { "Fira Code Nerd Font:style=Medium:size=14:antialias=true:hinting=true" };
 static const char dmenufont[]       = "Fira Code Nerd Font:style=Medium:size=14";
