b72f7e6b92b4d0829a41de48cabcd287578395d7
GG <weebcyberpunk@gmail.com>
2022-07-22 17:28:02 -0300

    Added symlink color config to st

---
+++ st-transparent/colors/light.h
@@ -0,0 +1,38 @@
+static const char *colorname[] = {
+	/* 8 normal colors */
+	"#5C5F77",
+	"#D20F39",
+	"#40A02B",
+	"#DF8E1D",
+	"#1E66F5",
+	"#EA76CB",
+	"#179299",
+	"#ACB0BE",
+
+	/* 8 bright colors */
+	"#6C6F85",
+	"#D20F39",
+	"#40A02B",
+	"#DF8E1D",
+	"#1E66F5",
+	"#EA76CB",
+	"#179299",
+	"#BCC0CC",
+
+
+
+[256] = "#4C4F69", /* default foreground colour */
+[257] = "#EFF1F5", /* default background colour */
+[258] = "#4C4F69", /*575268*/
+
+};
+
+
+/*
+ * foreground, background, cursor, reverse cursor
+ */
+unsigned int defaultfg = 256;
+unsigned int defaultbg = 257;
+unsigned int defaultcs = 258;
+static unsigned int defaultrcs = 258;
+
